28e50ae2e367e002286453a16942572d95666cf5
lingtian <83240757@qq.com>
2022-01-14 23:42:16 +0800

    Initial commit

---
+++ src/com/imti/dao/UserInfoDao.java
@@ -0,0 +1,12 @@
+package com.imti.dao;
+
+/**
+ * description: UserInfoDao <br>
+ * date: 2022/1/14 23:41 <br>
+ * author: anthony <br>
+ * version: 1.0 <br>
+ */
+public interface UserInfoDao {
+
+    public int addUserInfo();
+}
