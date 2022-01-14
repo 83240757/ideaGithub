6be6a5ad5d6c088b5760ddb2fe069e11e0a8da9e
lingtian <83240757@qq.com>
2022-01-14 23:40:05 +0800

    Initial commit

---
+++ src/com/imti/model/UserIfo.java
@@ -0,0 +1,15 @@
+package com.imti.model;
+
+/**
+ * description: UserIfo <br>
+ * date: 2022/1/14 23:39 <br>
+ * author: anthony <br>
+ * version: 1.0 <br>
+ */
+public class UserIfo {
+    //程序入口
+    public static void main(String[] args) {
+        System.out.println("用户信息" +
+                "");
+    }
+}
