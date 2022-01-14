5ac502d81a85e51179ce7977d1d81a27e06941bf
lingtian <83240757@qq.com>
2022-01-14 23:35:13 +0800

    Initial commit

---
+++ src/com/imti/model/IdeaGithub.java
@@ -0,0 +1,14 @@
+package com.imti.model;
+
+/**
+ * description: ideagithub <br>
+ * date: 2022/1/14 23:30 <br>
+ * author: anthony <br>
+ * version: 1.0 <br>
+ */
+public class IdeaGithub {
+    //程序入口
+    public static void main(String[] args) {
+        System.out.println("欢迎github");
+    }
+}
